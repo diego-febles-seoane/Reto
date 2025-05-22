5413501fdb514b8582cd8cbb20fd1235756fc2ff
Daniel Alejandro Rodríguez Herrera <danielrh111005@gmail.com>
2025-05-22 18:37:35 +0100

    operaciones usuario

--- reto-spring/src/main/java/es/reto/diheda/model/controllers/UsuarioController.java
+++ reto-spring/src/main/java/es/reto/diheda/model/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,10 @@ import org.springframework.web.server.ResponseStatusException;
 
 import es.reto.diheda.model.entities.Usuario;
 import es.reto.diheda.model.services.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -32,12 +37,19 @@ public class UsuarioController {
         this.usuarioService = userService;
     }
 
+    @Operation(summary = "Get all usuarios", description = "Returns all usuarios", responses = {
+		@ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(schema = @Schema(implementation = Message.class))) }
+	)
     @GetMapping("/users/")
     public List<Usuario> getAllUsuarios() {
         return usuarioService.getAllUsuarios();
     }
 
-
+    @Operation(summary = "Get a Usuario by id", description = "Return a usuario segun su id", responses = {
+		@ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(schema = @Schema(implementation = Message.class))),
+		@ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+		@ApiResponse(responseCode = "404", description = "Usuario not found") }
+	)
     @GetMapping("/user/{id}")
     public ResponseEntity<Usuario> getUsuarioById(@PathVariable(value = "id") int userId) {
         Usuario user = usuarioService.getUsuarioById(userId);
@@ -45,6 +57,11 @@ public class UsuarioController {
     }
 
 
+    @Operation(summary = "Adds a Usuario", description = "Add a Usuario", responses = {
+		@ApiResponse(responseCode = "200", description = "Successful operation"),
+		@ApiResponse(responseCode = "400", description = "Invalid data"),
+		@ApiResponse(responseCode = "409", description = "Already exists") }
+	)
     @PostMapping("/add/user/")
     public Usuario createUsuario(@Valid @RequestBody Usuario user) {
         if (user==null) {
@@ -57,6 +74,12 @@ public class UsuarioController {
     }
 
 
+
+    @Operation(summary = "Updates a Usuario by id", description = "Updates a Usuario", responses = {
+		@ApiResponse(responseCode = "200", description = "Successful operation"),
+		@ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+		@ApiResponse(responseCode = "404", description = "Usuario not found") }
+	)
     @PutMapping("/update/user/{id}")
     public ResponseEntity<Usuario> updateUsuario(@PathVariable(value = "id") int userId, @Valid @RequestBody Usuario userDetails) {
         final Usuario updatedUsuario = usuarioService.updateUsuario(userId, userDetails);
@@ -64,6 +87,11 @@ public class UsuarioController {
     }
 
 
+    @Operation(summary = "Deletes a Usuario by id", description = "Delete a Usuario", responses = {
+		@ApiResponse(responseCode = "200", description = "Successful operation"),
+		@ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+		@ApiResponse(responseCode = "404", description = "Usuario not found") }
+	)
     @DeleteMapping("/delete/user/{id}")
     public Map<String, Boolean> deleteUsuario(@PathVariable(value = "id") int userId) {
         usuarioService.deleteUsuario(userId);
